4c9312c13b0dbcf7141e38f54f97b13d972ab205
mk61199 <kamblemitali55@gmail.com>
2022-11-02 00:26:14 +0530

    Leave Application

---
+++ leave.py
@@ -0,0 +1,15 @@
+name=input("Teacher's name: ")
+
+Date=input("Date of application: ")
+
+Tenure=input("Leave Tenure: ")
+
+std=input("Class: ")
+
+Div=input("Section: ")
+Subject=input("Subject Line: ")
+
+Reason=input("Reason: ")
+
+
+print("Application Submitted") 
